8f6bd80a3c26c3d5a509443bb931fe7da21bbc77
M3L-N1K <93345052+M3L-N1K@users.noreply.github.com>
2021-11-01 17:58:42 +0300

    T-01
    FormText

---
+++ src/com/company/main/Main.java
@@ -0,0 +1,12 @@
+package com.company.main;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int y = 0;
+        int x;
+        x = y;
+        y++;
+        System.out.print(x + " " + y);
+    }
+}
